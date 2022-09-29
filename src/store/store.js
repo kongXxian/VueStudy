cb2adcd617b561da033295f5a0a62b565ed4712a
kongXxian <2839506683@qq.com>
2022-09-29 15:58:47 +0800

    vux数据持久化

--- src/store/store.js
+++ src/store/store.js
@@ -1,6 +1,9 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
 
+//数据持久化
+import createPersistedState from "vuex-persistedstate";
+
 Vue.use(Vuex)
 
 const store = new Vuex.Store({
@@ -55,7 +58,21 @@ const store = new Vuex.Store({
     delCompleted(state,payload){
       state.todos=state.todos.filter(ele=>!ele.completed)
     }
-  }
+  },
+  /* vuex数据持久化配置 */
+  plugins: [
+    createPersistedState({
+      // 存储方式：localStorage、sessionStorage、cookies
+      storage: window.sessionStorage,
+      // 存储的 key 的key值
+      key: "store",
+      render(state) {
+        // 要存储的数据：本项目采用es6扩展运算符的方式存储了state中所有的数据
+        return { ...state };
+      }
+    })
+  ]
+
 })
 
 export default store
